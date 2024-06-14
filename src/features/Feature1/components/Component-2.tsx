1a135df58588fc5865875fd7931f0a59fa2dd1a1
Marek Sobieraj <marek.sobieraj@contractor.qiagen.com>
2024-06-14 11:55:41 +0200

    prepared demo structure

--- src/features/Feature1/components/Component-2.tsx
+++ src/features/Feature1/components/Component-2.tsx
@@ -1,12 +1,9 @@
-import { Component1_1 } from "@features/Feature1/components/component-1/components/component-1-1/Component-1-1";
 import { feature_1_helper_2 } from "@features/Feature1/helpers/feature-1-helper-2/feature-1-helper-2";
 import { feature_1_helper_1 } from "@features/Feature1/helpers/feature-1-helper-1";
 import { feature_1_helper_2_1 } from "@features/Feature1/helpers/feature-1-helper-2/helpers/feature-1-helper-2-1";
 
 export const Component2 = (): void => {
   console.log("I'm a Component-1 ");
-
-  Component1_1();
 
   // can import feature helpers
   feature_1_helper_1();
